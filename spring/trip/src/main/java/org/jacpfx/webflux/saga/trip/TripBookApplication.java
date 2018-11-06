8ec9529740ab6bebdd643e43ff275d266d00858a
Andy Moncsek <amo.ahcp@gmail.com>
2018-11-06 18:02:02 +0100

    update spring saga example

---
+++ spring/trip/src/main/java/org/jacpfx/webflux/saga/trip/TripBookApplication.java
@@ -0,0 +1,12 @@
+package org.jacpfx.webflux.saga.trip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripBookApplication.class, args);
+	}
+}
